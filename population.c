d6771c53e67e7235829c74070a785d5943d12ebf
AnanoBukvaidze <116805741+AnanoBukvaidze@users.noreply.github.com>
2023-12-04 14:38:20 -0800

    Add files via upload
    week 1

---
+++ population.c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    int start_size;
+    do
+    {
+        start_size = get_int("Starting Population Size: ");
+    }
+    while (start_size < 9);
+
+    // TODO: Prompt for end size
+    int end_size;
+    do
+    {
+        end_size = get_int("Ending Population Size: ");
+    }
+    while (start_size > end_size);
+
+    // TODO: Calculate number of years until we reach threshold
+    int calculated_year = 0;
+    while (start_size < end_size)
+    {
+        start_size = start_size + (start_size / 3) - (start_size / 4);
+        calculated_year++;
+    }
+
+
+    // TODO: Print number of years
+    printf("Years: %i", calculated_year);
+}
